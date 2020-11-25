c380704377ba191d7721f62082f5ecd1e605da33
CotuSieDziejee <kam.gro@o2.pl>
2020-11-25 18:17:54 +0100

    paginator

--- CmentarzKomunalny/CmentarzKomunalny.Web/ClientApp/src/app/aktualnosci/aktualnosci/aktualnosci.component.ts
+++ CmentarzKomunalny/CmentarzKomunalny.Web/ClientApp/src/app/aktualnosci/aktualnosci/aktualnosci.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-aktualnosci',
   templateUrl: './aktualnosci.component.html',
-  styleUrls: ['./aktualnosci.component.css']
+  styleUrls: ['./aktualnosci.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AktualnosciComponent implements OnInit {
+
+  list = [];
+  pageSize = 5;
+  tempList = [];
 
   constructor() { }
 
   ngOnInit() {
+    // get your list from api
+    for (let i = 1; i < 1000; i++) {
+      this.list.push({
+        title: "item " + i
+      });
+    }
+
+    this.tempList = this.list.slice(0, this.pageSize);
   }
 
+  onPageChange(e) {
+    this.tempList = this.list.slice(e.pageIndex * e.pageSize, (e.pageIndex + 1) * e.pageSize);
+  }
   ngAfterViewInit() {
     document.querySelector('body').classList.add('active');
   }
--- CmentarzKomunalny/CmentarzKomunalny.Web/ClientApp/src/app/nekrologi/nekrologi/nekrologi.component.ts
+++ CmentarzKomunalny/CmentarzKomunalny.Web/ClientApp/src/app/nekrologi/nekrologi/nekrologi.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-nekrologi',
   templateUrl: './nekrologi.component.html',
-  styleUrls: ['./nekrologi.component.css']
+  styleUrls: ['./nekrologi.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NekrologiComponent implements OnInit {
+
+  list = [];
+  pageSize = 5;
+  tempList = [];
 
   constructor() { }
 
   ngOnInit() {
+    // get your list from api
+    for (let i = 1; i < 1000; i++) {
+      this.list.push({
+        title: "item " + i
+      });
+    }
+
+    this.tempList = this.list.slice(0, this.pageSize);
+  }
+
+  onPageChange(e) {
+    this.tempList = this.list.slice(e.pageIndex * e.pageSize, (e.pageIndex + 1) * e.pageSize);
   }
 
 }
